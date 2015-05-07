d4a138998d7571035b1c9833c849305da2e9fb20
DannyTran <dungtqa@gmail.com>
2015-05-07 17:16:20 +0700

    Update script

--- update.sh
+++ update.sh
@@ -7,4 +7,6 @@ rm vendor/assets/bower.js
 sudo -u app -H rake RAILS_ENV=production assets:precompile
 sudo -u app -H RAILS_ENV=production bin/delayed_job restart
 
+sudo -u app -H whenever --update-cron
+
 touch tmp/restart.txt
