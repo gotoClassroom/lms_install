302edaacb8f2399de3324ff93c0db0a97d7b0ed3
DannyTran <dungtqa@gmail.com>
2015-05-11 08:59:03 +0700

    Update script

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ sudo apt-get install gcc
 
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker|grep -c "ok installed")
 echo Checking for docker: $PKG_OK
-if [ "" == "$PKG_OK" ] or [ 0 == $PKG_OK ]; then
+if [ 0 == $PKG_OK ]; then
   echo "No docker. Install docker."
   wget -qO- https://get.docker.com/ | sh
 fi
 
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' fig|grep -c "ok installed")
 echo Checking for fig: $PKG_OK
-if [ "" == "$PKG_OK" ] or [ 0 == $PKG_OK ]; then
+if [ 0 == $PKG_OK ]; then
   echo "No fig. Install fig."
   curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
 fi
@@ -24,14 +24,15 @@ chmod +x  mysql/update.sh
 
 ./mysql/update.sh
 
-git clone https://github.com/Pithikos/docker-enter.git docker-enter
+if [ -f /usr/bin/docker-enter ]; then
+  echo "Docker enter exist!"
+else
+  git clone https://github.com/Pithikos/docker-enter.git docker-enter
 
-gcc docker-enter/docker-enter.c -o docker-enter/docker-enter
+  gcc docker-enter/docker-enter.c -o docker-enter/docker-enter
 
-sudo rm -rf /usr/bin/docker-enter
-
-sudo mv ./docker-enter/docker-enter /usr/bin
-
+  sudo mv ./docker-enter/docker-enter /usr/bin
+fi
 
 sudo fig build --no-cache
 
